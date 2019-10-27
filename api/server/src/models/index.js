1640ea196a4b5dd88ee8adbf901fb15fc60ae468
Hemant Achhami <hemantachhami19@gmail.com>
2019-10-27 20:58:00 +0545

    installing dotenv package to set up environment variables

--- api/server/src/models/index.js
+++ api/server/src/models/index.js
@@ -9,11 +9,30 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.environment === 'production') {
+  sequelize = new Sequelize(
+      process.env[config.use_env_variable], config
+  );
+  sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        dialectOption: {
+          ssl: true,
+          native: true
+        },
+        logging: true
+      }
+  );
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+      config.database, config.username, config.password, config
+  );
 }
+
 
 fs
   .readdirSync(__dirname)
