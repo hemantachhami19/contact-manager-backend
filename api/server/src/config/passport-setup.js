5d0e3662c2e7b95fb4065889e235f8c2e51b71e1
Hemant Achhami <hemantachhami19@gmail.com>
2019-11-03 12:07:16 +0545

    facebook callback url added

--- api/server/src/config/passport-setup.js
+++ api/server/src/config/passport-setup.js
@@ -25,7 +25,7 @@ passport.use(
     {
       clientID: keys.FACEBOOK_CONSUMER_KEY,
       clientSecret: keys.FACEBOOK_CONSUMER_SECRET,
-      callbackURL: "http://localhost:8000/api/v1/auth/facebook/callback"
+      callbackURL: keys.FACEBOOK_CALLBACK_URL
     },
     async (token, tokenSecret, profile, done) => {
       const currentUser = await database.User.findOne({
